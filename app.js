b8505088c1c9783319593331c2b09444a1b9a34c
Falk Lehmann <falk.lehmann.bp@nttdata.com>
2016-11-26 18:51:11 +0100

    entry 1st version.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var express = require('express')
+var url = require('url');
+
+var blobSvc = require('azure-storage').createBlobService();
+var documentClient = require("documentdb").DocumentClient;
+
+var config = {}
+config.endpoint = "https://neuenaehentt.documents.azure.com:443/";
+config.primaryKey = "wSWrRxtVz8aXiuI9iy4lHNIe5Bi5pdWHYBciBqjYgjuR6oLtEQ9XurCGMKIWBHlWWVkgdSM7JSJ34uHtBBIhgw==";
+	
+var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+
 var app = express()
 
 app.get('/', function (req, res) {
@@ -7,6 +18,76 @@ app.get('/', function (req, res) {
 	res.send('Hello World!')
 })
 
+app.post('/entry', function(req, res) {
+	saveEntry(req, res);
+})
+
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT)
 })
+
+function saveEntry(req, res) {
+	var containerName = 'entries';
+
+	blobSvc.createContainerIfNotExists(containerName, function(error, result, response){
+		if (error) {
+			console.log("Couldn't create container %s", containerName);
+			console.error(err);
+			
+			res.status(500).send('Could not find or create container "' + containerName + '"!')
+			
+			return;
+		} else {
+			if (result) {
+				console.log('Container %s created', containerName);
+			} else {
+				console.log('Container %s already exists', containerName);
+			}
+		}
+	});
+	
+	var usr = req.param("user");
+	var buf = new Buffer(req.param("img"), 'base64');
+	var lat = req.param("lat");
+	var lng = req.param("long");
+	
+	var fileName = 'place-' + lat + '-' + lng;
+	
+	blobSvc.createBlockBlobFromText(containerName, fileName, buf, function (error, result, response) {
+		if(error){
+            console.log("Couldn't not store file as text '" + fileName + "!");
+            console.error(error);
+			
+			res.status(500).send('Could not store file as text "' + fileName + '"!')
+        } else {
+            console.log('String uploaded successfully');
+        }
+	});
+	
+
+	// "EntryDB"
+	// "EntryCollection"
+	// var HttpStatusCodes = { NOTFOUND: 404 };
+	var collectionUrl = 'dbs/EntryDB/colls/EntryCollection';
+
+	var document = {
+		"filename": fileName,
+		"user": usr,
+		"lat" : lat,
+		"lng" : lng,
+		"timestamp" : new Date().getMilliseconds
+	};
+
+	documentClient.createDocument(collectionUrl, document, (err, created) => {
+				if (err) {
+					console.log("Couldn't not store document for '" + fileName + "!");
+					console.error(error);
+
+					res.status(500).send('Could not store document for "' + fileName + '"!')
+				}
+				else {
+					console.log('Document saved successfully');
+				}
+		});
+
+}
